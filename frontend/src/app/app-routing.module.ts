f1ec272b195fc67aede3f2e34730babd0eaf2242
unknown <yashwant.police@gmail.com>
2023-08-01 12:41:17 +0530

    added ui

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { TrackComponent } from './track/track.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: LandingComponent },
+  { path: 'track', component: TrackComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
